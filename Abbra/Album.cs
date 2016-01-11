eec95508da186de9f4ead60c7cc2488ed0792fc3
linaZaytseva <elinazaytseva@rambler.ru>
2016-01-11 16:29:08 +0300

    Entities

---
+++ Abbra/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbra
+{
+    class Album
+    {
+        string name;
+        List<string> songs;
+
+        public Album(string name, List<string> songs)
+        {
+            this.name = name;
+            this.songs = songs;
+        }
+    }
+}
---
+++ Abbra/Singer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbra
+{
+    class Singer
+    {
+        string singerName;
+        Album albumName;
+
+        public Singer(string singerName, Album albumName)
+        {
+            this.singerName = singerName;
+            this.albumName = albumName;
+        }
+    }
+}
